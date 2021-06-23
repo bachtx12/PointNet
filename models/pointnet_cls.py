1671cb6d8646fa5dc64fd0c761d7456cf433e258
bachtx12 <v.bachtx12@vinai.io>
2021-06-23 10:53:25 +0700

    temp

--- models/pointnet_cls.py
+++ models/pointnet_cls.py
@@ -63,7 +63,66 @@ class PointNet(nn.Module):
 
         x = self.classifier(x)
         return F.log_softmax(x, dim=1), T1, T2
+class PointNet_global(nn.Module):
+    def __init__(self, input_channels, num_classes, feature_dim=128):
+        super(PointNet_global, self).__init__()
+        self.input_channels = input_channels
+        self.stn1 = STN3D_input(input_channels)
+        self.stn2 = STN3D_feature(64)
+        self.feature_dim=feature_dim
+        self.num_classes = num_classes
+        self.mlp1 = nn.Sequential(
+            nn.Conv1d(input_channels, 64, 1),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Conv1d(64, 64, 1),
+            nn.BatchNorm1d(64),
+            nn.ReLU()
+        )
+        self.mlp2 = nn.Sequential(
+            nn.Conv1d(64, 64, 1),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Conv1d(64, 128, 1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Conv1d(128, 1024, 1),
+            nn.BatchNorm1d(1024),
+            nn.ReLU()
+        )
 
+        self.classifier = nn.Sequential(
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(p=0.3),
+            nn.Linear(512, 256),
+            # nn.Dropout(p=0.3),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Dropout(p=0.3),
+            nn.Linear(256, self.num_classes)
+        )
+        self.global_project = nn.Sequential(
+            nn.Linear(1024, 512, bias=False), 
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, self.feature_dim, bias=True)
+        )
+    def forward(self, x):
+        batch_size = x.shape[0]
+        num_points = x.shape[1]
+        x = x.transpose(2, 1)
+        T1 = self.stn1(x)
+        x = torch.bmm(T1, x)
+        x = self.mlp1(x)
+        T2 = self.stn2(x)
+        f = torch.bmm(T2, x)
+        x = self.mlp2(f)
+        x = F.max_pool1d(x, num_points).squeeze(2)
+        global_feat_proj = self.global_project(x) 
+        x = self.classifier(x)
+        return F.log_softmax(x, dim=1), T1, T2, global_feat_proj
 def get_loss(pred, target, feat_trans, reg_weight=0.001):
     # cross entropy loss
     loss_cls = F.nll_loss(pred, target) 
