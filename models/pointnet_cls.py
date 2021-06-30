6c7abbcd962e50042cafe39eec1e5f8f81f873aa
bachtx12 <v.bachtx12@vinai.io>
2021-06-30 11:16:26 +0700

    add svm

--- models/pointnet_cls.py
+++ models/pointnet_cls.py
@@ -10,12 +10,15 @@ sys.path.append(os.path.join(BASE_DIR, 'models'))
 sys.path.append(os.path.join(BASE_DIR, 'utils'))
 from pointnet_utils import feature_transform_regularizer, STN3D_input, STN3D_feature
 class PointNet(nn.Module):
-    def __init__(self, input_channels, num_classes):
+    def __init__(self, input_channels, num_classes, global_feature=False, feature_transform=True):
         super(PointNet, self).__init__()
         self.input_channels = input_channels
         self.stn1 = STN3D_input(input_channels)
-        self.stn2 = STN3D_feature(64)
+        if self.feature_transform:
+            self.stn2 = STN3D_feature(64)
         self.num_classes = num_classes
+        self.global_feature = global_feature
+        self.feature_transform = feature_transform
         self.mlp1 = nn.Sequential(
             nn.Conv1d(input_channels, 64, 1),
             nn.BatchNorm1d(64),
@@ -56,21 +59,26 @@ class PointNet(nn.Module):
         T1 = self.stn1(x)
         x = torch.bmm(T1, x)
         x = self.mlp1(x)
-        T2 = self.stn2(x)
-        f = torch.bmm(T2, x)
-        x = self.mlp2(f)
+        if self.feature_transform:
+            T2 = self.stn2(x)
+            f = torch.bmm(T2, x)
+            x = self.mlp2(f)
+        else:
+            x = self.mlp2(x)
+            T2 = None
         x = F.max_pool1d(x, num_points).squeeze(2)
-
+        if self.global_feature:
+            return x
         x = self.classifier(x)
         return F.log_softmax(x, dim=1), T1, T2
-class PointNet_global(nn.Module):
-    def __init__(self, input_channels, num_classes, feature_dim=128):
-        super(PointNet_global, self).__init__()
+class PointNet_critical(nn.Module):
+    def __init__(self, input_channels, feature_transform=True):
+        super(PointNet_critical, self).__init__()
         self.input_channels = input_channels
         self.stn1 = STN3D_input(input_channels)
-        self.stn2 = STN3D_feature(64)
-        self.feature_dim=feature_dim
-        self.num_classes = num_classes
+        self.feature_transform = feature_transform
+        if self.feature_transform:
+            self.stn2 = STN3D_feature(64)
         self.mlp1 = nn.Sequential(
             nn.Conv1d(input_channels, 64, 1),
             nn.BatchNorm1d(64),
@@ -91,24 +99,6 @@ class PointNet_global(nn.Module):
             nn.ReLU()
         )
 
-        self.classifier = nn.Sequential(
-            nn.Linear(1024, 512),
-            nn.BatchNorm1d(512),
-            nn.ReLU(),
-            nn.Dropout(p=0.3),
-            nn.Linear(512, 256),
-            # nn.Dropout(p=0.3),
-            nn.BatchNorm1d(256),
-            nn.ReLU(),
-            nn.Dropout(p=0.3),
-            nn.Linear(256, self.num_classes)
-        )
-        self.global_project = nn.Sequential(
-            nn.Linear(1024, 512, bias=False), 
-            nn.BatchNorm1d(512),
-            nn.ReLU(inplace=True),
-            nn.Linear(512, self.feature_dim, bias=True)
-        )
     def forward(self, x):
         batch_size = x.shape[0]
         num_points = x.shape[1]
@@ -116,13 +106,16 @@ class PointNet_global(nn.Module):
         T1 = self.stn1(x)
         x = torch.bmm(T1, x)
         x = self.mlp1(x)
-        T2 = self.stn2(x)
-        f = torch.bmm(T2, x)
-        x = self.mlp2(f)
-        x = F.max_pool1d(x, num_points).squeeze(2)
-        global_feat_proj = self.global_project(x) 
-        x = self.classifier(x)
-        return F.log_softmax(x, dim=1), T1, T2, global_feat_proj
+        if self.feature_transform:
+            T2 = self.stn2(x)
+            f = torch.bmm(T2, x)
+            x = self.mlp2(f)
+        else:
+            T2 = None
+            x = self.mlp2(x)
+        critical_index = torch.argmax(x, dim=2)
+        global_feature = F.max_pool1d(x, num_points).squeeze(2)
+        return global_feature, critical_index, x
 def get_loss(pred, target, feat_trans, reg_weight=0.001):
     # cross entropy loss
     loss_cls = F.nll_loss(pred, target) 
